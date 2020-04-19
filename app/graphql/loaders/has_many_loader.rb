da31fc36b147cc63c0cbe538152cb399e9d156ac
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2020-04-18 20:55:34 -0400

    ✨  Sort has_many relations on graphql types

--- app/graphql/loaders/has_many_loader.rb
+++ app/graphql/loaders/has_many_loader.rb
@@ -1,13 +1,16 @@
 class Loaders::HasManyLoader < GraphQL::Batch::Loader
-  def initialize(model, column)
+  def initialize(model, column, sort: nil)
     @model = model
     @column = column
+    @sort = sort
   end
 
   def perform(relation_ids)
-    filter = { @column => relation_ids.uniq }
+    query = @model
+    query = query.where({ @column => relation_ids.uniq })
+    query = query.order(@sort) if @sort
 
-    records_by_relation_id = @model.where(filter).group_by do |result|
+    records_by_relation_id = query.group_by do |result|
       result.public_send(@column)
     end
 
