d129cdb0138821258294bf509237e05a1ee44ccf
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2020-04-18 20:26:01 -0400

    ✨  Limit graphql queries

--- app/graphql/types/base_field.rb
+++ app/graphql/types/base_field.rb
@@ -1,5 +1,22 @@
 module Types
   class BaseField < GraphQL::Schema::Field
     argument_class Types::BaseArgument
+
+    def limit_argument(default: 10, min: 0, max: 100)
+      argument(
+        :limit,
+        Integer,
+        required: false,
+        default_value: default,
+        prepare: ->(value, _ctx) {
+          if value && value.between?(min, max)
+            value
+          else
+            message = "'limit' must be between #{min} and #{max}"
+            raise GraphQL::ExecutionError, message
+          end
+        }
+      )
+    end
   end
 end
