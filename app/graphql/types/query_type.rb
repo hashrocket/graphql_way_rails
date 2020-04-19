40f127371d8368744e7afd17e60eda866ad8698a
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2020-04-18 20:26:01 -0400

    ✨  Limit graphql queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,22 +1,41 @@
 class Types::QueryType < Types::BaseObject
-  field :categories, [Types::CategoryType], null: false
-  field :products, [Types::ProductType], null: false
-  field :users, [Types::UserType], null: false
-  field :orders, [Types::OrderType], null: false
-
-  def categories
-    Category.all
+  field :categories, [Types::CategoryType], null: false do
+    limit_argument
   end
 
-  def products
-    Product.all
+  field :products, [Types::ProductType], null: false do
+    limit_argument
   end
 
-  def users
-    User.all
+  field :users, [Types::UserType], null: false do
+    limit_argument
   end
 
-  def orders
-    Order.all
+  field :orders, [Types::OrderType], null: false do
+    limit_argument
+  end
+
+  def categories(limit: nil)
+    query = Category
+    query = query.limit(limit) if limit
+    query
+  end
+
+  def products(limit: nil)
+    query = Product
+    query = query.limit(limit) if limit
+    query
+  end
+
+  def users(limit: nil)
+    query = User
+    query = query.limit(limit) if limit
+    query
+  end
+
+  def orders(limit: nil)
+    query = Order
+    query = query.limit(limit) if limit
+    query
   end
 end
