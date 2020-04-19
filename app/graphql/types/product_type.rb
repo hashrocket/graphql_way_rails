5ff29cacbefcf7316e5181afa4f7e263ab4c6ef5
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2020-04-18 22:32:05 -0400

    ✨  Sort graphql has_many through relations

--- app/graphql/types/product_type.rb
+++ app/graphql/types/product_type.rb
@@ -4,14 +4,18 @@ class Types::ProductType < Types::BaseObject
   field :size, String, null: true
   field :price_cents, Integer, null: false
   field :category, Types::CategoryType, null: false
-  field :orders, [Types::OrderType], null: false
+  field :orders, [Types::OrderType], null: false do
+    sort_argument :ordered_at
+  end
 
   def category
     Loaders::BelongsToLoader.for(Category).load(object.category_id)
   end
 
-  def orders
-    order_items.then do |order_item_list|
+  def orders(sort: nil)
+    sort = sort.map{ |k, v| ["orders.#{k}", v] }.to_h
+
+    order_items(joins: :order, sort: sort).then do |order_item_list|
       order_ids = order_item_list.map(&:order_id)
       Loaders::BelongsToLoader.for(Order).load_many(order_ids)
     end
@@ -23,7 +27,7 @@ class Types::ProductType < Types::BaseObject
 
   private
 
-  def order_items
-    Loaders::HasManyLoader.for(OrderItem, :product_id).load(object.id)
+  def order_items(*options)
+    Loaders::HasManyLoader.for(OrderItem, :product_id, *options).load(object.id)
   end
 end
