993e517d5f066c1303dab50dfaf7b4e59e6122ad
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2020-04-20 09:08:22 -0400

    ✨  Filter users by email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_many :orders
 
-  def self.graphql_query(joins: nil, sort: nil, limit: nil)
+  def self.graphql_query(email: nil, joins: nil, sort: nil, limit: nil)
     query = all
+    query = query.where(users: {email: email}) if email
     query = query.joins(joins) if joins
     query = query.order(sort) if sort
     query = query.limit(limit) if limit
