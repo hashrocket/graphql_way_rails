42df071c8175a0095d5c66ee008b669ccbd4c3e2
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2020-04-26 15:47:48 -0400

    🎨  Make sort arg on graphql more explicit on how to sort

--- app/graphql/types/base_field.rb
+++ app/graphql/types/base_field.rb
@@ -2,7 +2,7 @@ module Types
   class BaseField < GraphQL::Schema::Field
     argument_class Types::BaseArgument
 
-    def limit_argument(default: 10, min: 0, max: 100)
+    def limit_argument(default: 10, min: 1, max: 100)
       prepare = ->(value, _ctx) {
         if value&.between?(min, max)
           value
@@ -15,7 +15,7 @@ module Types
       argument(:limit, Integer, required: false, default_value: default, prepare: prepare)
     end
 
-    def sort_argument(table_name, fields_map)
+    def sort_argument(**fields_map)
       fields_map = fields_map.map { |k, v| [k.to_s, v] }.to_h
 
       prepare = ->(values, _ctx) {
@@ -23,7 +23,7 @@ module Types
           dir = value[0] == "-" ? :desc : :asc
           value = value[1..-1] if ["+", "-"].include?(value[0])
           if fields_map[value]
-            ["#{table_name}.#{fields_map[value]}", dir]
+            [fields_map[value], dir]
           else
             message = "'sort' must be included in [#{fields_map.keys.join(", ")}] prefixed by a plus or minus sign."
             raise GraphQL::ExecutionError, message
